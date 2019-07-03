cb5d7c11d1d07f2f684ccd5b2eabe2a8c6e23b59
Andrew Usher <andrewusher00@gmail.com>
2019-07-03 18:22:20 -0500

    Add loading indicator to birthdays screen

--- screens/Birthdays.js
+++ screens/Birthdays.js
@@ -1,32 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { ScrollView, View } from 'react-native'
+import { ActivityIndicator, ScrollView, View } from 'react-native'
 import { Card, ListItem } from 'react-native-elements'
-import AnimatedLinearGradient from 'react-native-svg-animated-linear-gradient'
-import SVG, { Circle, Rect } from 'react-native-svg'
-import styled from 'styled-components/native'
-
-const SkeletonContainer = styled.View`
-  padding: 10% 0;
-`
-
-const BirthdaySkeleton = () => (
-  <SkeletonContainer>
-    <AnimatedLinearGradient height={300}>
-      <SVG transform="translate(100,100)">
-        <Circle cx="30" cy="30" r="30" />
-        <Rect x="75" y="13" rx="4" ry="4" width="100" height="13" />
-        <Rect x="75" y="37" rx="4" ry="4" width="50" height="8" />
-        <Rect x="0" y="70" rx="5" ry="5" width="400" height="200" />
-      </SVG>
-    </AnimatedLinearGradient>
-  </SkeletonContainer>
-)
 
 const Birthdays = ({ navigation }) => {
   const [loading, setLoading] = useState(true)
   const [celebs, setCelebs] = useState([])
 
-  const day = navigation.getParam('day', '')
+  const day = navigation.getParam('day')
   useEffect(() => {
     fetch(
       `https://celeb-birthday-service.andrewusher00.now.sh/api/birthdays.js?day=${day}`
@@ -40,7 +20,7 @@ const Birthdays = ({ navigation }) => {
   }, [])
   return (
     <View>
-      {loading && <BirthdaySkeleton />}
+      {loading && <ActivityIndicator />}
       {!celebs.length ? null : (
         <ScrollView>
           <Card containerStyle={{ padding: 0 }}>
